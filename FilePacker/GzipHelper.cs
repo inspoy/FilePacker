39902d7f0aca9468da33fdd9e8a3e7a63a657fd9
inspoy <inspoy@163.com>
2019-04-04 00:13:00 +0800

    增加gz

---
+++ FilePacker/GzipHelper.cs
@@ -0,0 +1,50 @@
+using System.IO;
+using System.IO.Compression;
+
+namespace Instech.FilePacker
+{
+    internal static class GzipHelper
+    {
+        public static byte[] Compress(byte[] src)
+        {
+            byte[] buffer;
+            using (var ms = new MemoryStream())
+            {
+                using (var gs = new GZipStream(ms, CompressionMode.Compress, true))
+                {
+                    gs.Write(src, 0, src.Length);
+                }
+                buffer = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(buffer, 0, buffer.Length);
+            }
+            return buffer;
+        }
+        public static byte[] Decompress(byte[] src)
+        {
+            byte[] buffer;
+            using (var ms = new MemoryStream(src))
+            {
+                using (var gs = new GZipStream(ms, CompressionMode.Decompress, true))
+                {
+                    MemoryStream msreader = new MemoryStream();
+                    buffer = new byte[0x1000];
+                    while (true)
+                    {
+                        int reader = gs.Read(buffer, 0, buffer.Length);
+                        if (reader <= 0)
+                        {
+                            break;
+                        }
+                        msreader.Write(buffer, 0, reader);
+                    }
+
+                    msreader.Position = 0;
+                    buffer = msreader.ToArray();
+                    msreader.Close();
+                }
+            }
+            return buffer;
+        }
+    }
+}
